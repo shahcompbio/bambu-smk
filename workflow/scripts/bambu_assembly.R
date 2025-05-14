4f457b88d1083f18c5253a3774b2ed42c2e1ba3a
apsteinberg <asherps@gmail.com>
2025-05-14 15:17:29 -0400

    low memory mode for assembly

--- workflow/scripts/bambu_assembly.R
+++ workflow/scripts/bambu_assembly.R
@@ -24,6 +24,7 @@ bambu_out <- snakemake@params[["outdir"]]
 ##prep annotations ...
 fa.file <- snakemake@params[["ref_genome"]]
 gtf.file <- snakemake@params[["ref_gtf"]]
+yieldSize <- snakemake@params[["yieldsize"]]
 # This function creates a reference annotation object which is used for transcript discovery and quantification in Bambu.
 annotations <- prepareAnnotations(gtf.file)
 ## load in all the bam files in current directory ...
@@ -60,7 +61,7 @@ ncores <- length(rds)
 ## baselineFDR defaults to 0.1 when NDR is not selected ...
 ## try with model training to begin with as Andre Sim recommended ...
 se.NDR_default <- bambu(reads = rds, annotations = annotations, ncore = ncores,
-            genome = fa.file, verbose=TRUE,
+            genome = fa.file, verbose=TRUE, yieldSize = yieldSize, lowMemory=TRUE,
             fusionMode=FALSE)
 
 ## write outputs to gtf and expression level files
@@ -72,12 +73,14 @@ save(se.NDR_default, file = se_out)
 ### now let's test different NDR thresholds
 ## start by doing transcript discovery
 newAnnotations <- bambu(reads = rds, annotations = annotations, ncore = ncores,
-                        genome = fa.file, NDR = 1, verbose=TRUE, quant = FALSE)
+                        genome = fa.file, NDR = 1, verbose=TRUE,
+                        yieldSize = yieldSize, lowMemory=TRUE, quant = FALSE)
 ##################
 ### do quantification on all new transcripts (NDR = 1) ....
 ###########
 se.NDR_1 <- bambu(reads = rds, annotations = newAnnotations, ncore = ncores,
-                  genome = fa.file, verbose=TRUE, NDR = 1, discovery = FALSE)
+                  genome = fa.file, verbose=TRUE, yieldSize = yieldSize,
+                  lowMemory=TRUE, NDR = 1, discovery = FALSE)
 multi_out <- sprintf("%s/transcriptome_NDR_1/", bambu_out)
 create_directory(multi_out)
 ## write outputs to gtf and expression level files
@@ -91,7 +94,8 @@ save(se.NDR_1, file = se_out)
 ## make a filtered annotation
 annotations.filtered <- newAnnotations[(!is.na(mcols(newAnnotations)$NDR) & mcols(newAnnotations)$NDR<0.1) | is.na(mcols(newAnnotations)$NDR)]
 se.NDR_0.1 <- bambu(reads = rds, annotations = annotations.filtered, ncore = ncores,
-                    genome = fa.file, verbose=TRUE, NDR = 1, discovery = FALSE)
+                    genome = fa.file, verbose=TRUE, yieldSize = yieldSize,
+                    lowMemory=TRUE, NDR = 1, discovery = FALSE)
 multi_out <- sprintf("%s/transcriptome_NDR_0.1/", bambu_out)
 create_directory(multi_out)
 ## write outputs to gtf and expression level files
